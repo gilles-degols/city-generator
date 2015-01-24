d6f3b31dc0b0054f8ff8a5c80fc2d85f34f47c41
StarPlatinum <shotokan026@hotmail.com>
2015-01-24 23:28:05 +0100

    Modify Garden and Station

--- src/elements/stations/Station.py
+++ src/elements/stations/Station.py
@@ -5,4 +5,13 @@ class Station(object):
         unX = un_pos_x + (un_size_x - 1)/2
         unY = un_pos_y + (un_size_y - 1)/2
         
-        # Put/duplicate Airship from airship.blend at (unX, unY, un_pos_z + 4/3) * City.UNIT_VALUE
+		obj = bpy.data.objects['_Ornithopter']
+		mesh = obj.data
+		new_obj = bpy.data.objects.new('Ornithopter', mesh)
+		new_obj.location = (unX * City_UNIT_VALUE, unY * City_UNIT_VALUE, (un_pos_z + 4/3) * City_UNIT_VALUE)
+		
+		if un_size_x < un_size_y:
+			new_obj.rotation_mode = 'XYZ'
+			new_obj.rotation_euler = (0, 0, 90)
+		
+		scene.objects.link(new_obj)
