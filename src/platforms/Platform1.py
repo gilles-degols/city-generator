b6240c5f450e25a3ceb83cc7b572f9c92e95b7cc
StarPlatinum <shotokan026@hotmail.com>
2015-01-25 00:10:59 +0100

    Modify Platform1 and Platform2.

--- src/platforms/Platform1.py
+++ src/platforms/Platform1.py
@@ -6,4 +6,12 @@ class Platform1(object):
         fY = un_pos_y + (un_size_y - 1)/2
         
         # Put/duplicate mountain1 at (fX, fY, un_pos_z)*City...
-        # Scale it (un_size_x, un_size_y, min(un_size_x, un_size_y))
+		obj = bpy.data.objects['_Mountain1']
+		mesh = obj.data
+		new_obj = bpy.data.objects.new('Mountain1', mesh)
+		new_obj.location = (fX * City_UNIT_VALUE, fY * City_UNIT_VALUE, un_pos_z * City_UNIT_VALUE)
+		
+		# Scale it (un_size_x, un_size_y, min(un_size_x, un_size_y))
+		new_obj.scale = (un_size_x, un_size_y, min(un_size_x, un_size_y))
+		
+		scene.objects.link(new_obj)
--- src/platforms/Platform2.py
+++ src/platforms/Platform2.py
@@ -5,5 +5,13 @@ class Platform2(object):
         fX = un_pos_x + (un_size_x - 1)/2
         fY = un_pos_y + (un_size_y - 1)/2
         
-        # Put/duplicate mountain2 at (fX, fY, un_pos_z)*City...
-        # Scale it (un_size_x, un_size_y, min(un_size_x, un_size_y))
+        # Put/duplicate mountain1 at (fX, fY, un_pos_z)*City...
+		obj = bpy.data.objects['_Mountain2']
+		mesh = obj.data
+		new_obj = bpy.data.objects.new('Mountain2', mesh)
+		new_obj.location = (fX * City_UNIT_VALUE, fY * City_UNIT_VALUE, un_pos_z * City_UNIT_VALUE)
+		
+		# Scale it (un_size_x, un_size_y, min(un_size_x, un_size_y))
+		new_obj.scale = (un_size_x, un_size_y, min(un_size_x, un_size_y))
+		
+		scene.objects.link(new_obj)
