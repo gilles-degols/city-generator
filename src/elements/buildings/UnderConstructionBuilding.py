148a1676ad2bd68f0c10117c6348d9ea6ba855de
StarPlatinum <shotokan026@hotmail.com>
2015-01-25 22:26:10 +0100

    Apply scale in city.blend.

--- src/elements/buildings/UnderConstructionBuilding.py
+++ src/elements/buildings/UnderConstructionBuilding.py
@@ -1,33 +1,33 @@
 from random import random
 
-class UnderConstuctionBuilding(object):
+class UnderConstructionBuilding(object):
 
     def __init__(self, un_pos_x, un_pos_y, un_pos_z, un_size_x, un_size_y, un_height):
-		wall = bpy.data.objects['_UnderConstructionElement']
-		window = bpy.data.objects['_UnderConstructionWindow']
-		linkedBeams = bpy.data.objects['_UnderConstructionLinkedBeams']
-		beamPieces = bpy.data.objects['_UnderConstructionBeamPieces']
-		
-		new_obj = bpy.data.objects.new('UnderConstructionElement', wall.data)
-		new_obj.location = ((un_pos_x + (un_size_x - 1)/2) * City_UNIT_VALUE, (un_pos_y + (un_size_y - 1)/2) * City_UNIT_VALUE, un_pos_z * City_UNIT_VALUE)
-		new_obj.scale = (un_size_x, un_size_y, 1)
-        new_obj.select = True
-		scene.objects.link(new_obj)
+        wall = bpy.data.objects['_UnderConstructionElement']
+        window = bpy.data.objects['_UnderConstructionWindow']
+        linkedBeams = bpy.data.objects['_UnderConstructionLinkedBeams']
+        beamPieces = bpy.data.objects['_UnderConstructionBeamPieces']
         
-		for i in range(1, un_height - 2):
+        new_obj = bpy.data.objects.new('UnderConstructionElement', wall.data)
+        new_obj.location = ((un_pos_x + (un_size_x - 1)/2) * City.UNIT_VALUE, (un_pos_y + (un_size_y - 1)/2) * City.UNIT_VALUE, un_pos_z * City.UNIT_VALUE)
+        new_obj.scale = (un_size_x, un_size_y, 1)
+        new_obj.select = True
+        scene.objects.link(new_obj)
+        
+        for i in range(1, un_height - 2):
             new_obj = bpy.data.objects.new('UnderConstructionWindow', window.data)
-			new_obj.location = ((un_pos_x + (un_size_x - 1)/2) * City_UNIT_VALUE, (un_pos_y + (un_size_y - 1)/2) * City_UNIT_VALUE, (un_pos_z + i) * City_UNIT_VALUE)
-			new_obj.scale = (un_size_x, un_size_y, 1)
+            new_obj.location = ((un_pos_x + (un_size_x - 1)/2) * City.UNIT_VALUE, (un_pos_y + (un_size_y - 1)/2) * City.UNIT_VALUE, (un_pos_z + i) * City.UNIT_VALUE)
+            new_obj.scale = (un_size_x, un_size_y, 1)
             new_obj.select = True
-			scene.objects.link(new_obj)
-		
-        for j in range(un_height - 1, un_height)
-			for k in range(un_pos_x, un_size_x - 1):
-				for l in range(un_pos_y, un_size_y - 1):
-					new_obj = bpy.data.objects.new('UnderConstructionLinkedBeams', linkedBeams.data)
-					new_obj.location = (k * City_UNIT_VALUE, l * City_UNIT_VALUE, (un_pos_z + j) * City_UNIT_VALUE)
+            scene.objects.link(new_obj)
+        
+        for j in range(un_height - 1, un_height):
+            for k in range(un_pos_x, un_size_x - 1):
+                for l in range(un_pos_y, un_size_y - 1):
+                    new_obj = bpy.data.objects.new('UnderConstructionLinkedBeams', linkedBeams.data)
+                    new_obj.location = (k * City.UNIT_VALUE, l * City.UNIT_VALUE, (un_pos_z + j) * City.UNIT_VALUE)
                     new_obj.select = True
-					scene.objects.link(new_obj)
+                    scene.objects.link(new_obj)
             
         # Join all objects and remove doubles
         scene.objects.active = new_obj
